f56f0517c85c28dbf07d99fbd6a2497bd4981d8a
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-08-16 20:23:08 -0300

    feat: criei o projeto

---
+++ PAMNotes/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace PAMNotes
+{
+    public partial class MainPage : ContentPage
+    {
+        string filePath = Path.Combine(FileSystem.AppDataDirectory,"Notes.txt");
+        public MainPage()
+        {
+            InitializeComponent();
+            if (File.Exists(filePath))
+            {
+                NoteEditor.Text = File.ReadAllText(filePath);
+            }
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            string texto = NoteEditor.Text;
+            File.WriteAllText(filePath, texto);
+            DisplayAlert("Sucesso", "Operação concluida!", "Ok");
+
+        }
+
+        private void DeleteButton_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                DisplayAlert("Sucesso", "Operação concluida com sucesso!", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Arquivo não encontrado", "Ok");
+            }
+        }
+    }
+}
