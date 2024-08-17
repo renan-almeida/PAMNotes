443991297b129031afb18ce673a71af64b33bde2
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-08-16 22:32:55 -0300

    feat: Adicionei a AboutPage. Adicionei TabBar

---
+++ PAMNotes/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+namespace PAMNotes;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void AboutButton_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://github.com/DS-ETECHAS");
+    }
+}
